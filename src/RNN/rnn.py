33eb9ec323506b483bc6bd8203444414d764873c
robPTY <rcadown8@gmail.com>
2025-11-25 16:59:50 -0500

    added epochs, loss calc

--- src/RNN/rnn.py
+++ src/RNN/rnn.py
@@ -1,10 +1,12 @@
 import torch
 import pandas as pd
+from typing import Tuple
 import torch.nn.functional as F
 
 INPUT_DIMENSIONS = (1, 2)
 HIDDEN_DIMENSIONS = (2, 2)
 OUTPUT_DIMENSIONS = (2, 1)
+TIME_STEPS = 3
 
 class RNN:
     def __init__(self):
@@ -14,48 +16,81 @@ class RNN:
         self.OutputWeights = torch.randn(OUTPUT_DIMENSIONS, generator=gen)
         self.HiddenBias = torch.randn(HIDDEN_DIMENSIONS[1], generator=gen)
         self.OutputBias = torch.randn(OUTPUT_DIMENSIONS[1], generator=gen)
+        self.epochs = 100
 
-    def forward_pass(network, temperatures):
+    def forward_pass(self, X_seq: torch.tensor, Y: torch.tensor) -> Tuple[torch.tensor, torch.tensor]:
         previous_step = None
-        logits = torch.zeros((100, 1))
-        for t in range(100):
+        hidden = torch.zeros((X_seq.shape[0], self.InputWeights.shape[1]))
+        outputs = torch.zeros((X_seq.shape[0], self.OutputWeights.shape[1]))
+
+        for t in range(X_seq.shape[0]):
             # Input layer
-            X = torch.tensor(temperatures[t]).unsqueeze((0))
-            X_i = X @ network.InputWeights
+            X = X_seq[t].unsqueeze((0))
+            X_i = X @ self.InputWeights
 
             # Hidden layer
             if previous_step is None: 
                 Xh_t = F.relu(X_i)
             else:
-                X_h = previous_step @ network.HiddenWeights
-                Xh_t = F.tanh(X_i + X_h + network.HiddenBias)
+                X_h = previous_step @ self.HiddenWeights
+                Xh_t = F.tanh(X_i + X_h + self.HiddenBias)
             previous_step = Xh_t
 
-            print(f'hidden state: {Xh_t}')
+            hidden[t, :] = Xh_t
             
             # Output layer
-            logit = Xh_t @ network.OutputWeights + network.OutputBias
-            logits[t] = logit
-            print(f'at time t={t}, temperature is {logit}')
+            logit = Xh_t @ self.OutputWeights + self.OutputBias # Y_hat
+            outputs[t, :] = logit 
         
-        counts = logits.exp() 
-        dimension = 1
-        probabilities = counts / counts.sum(dimension, keepdim=True)
-        return 
+        # Return the hidden state for grad descent, output of the RNN 
+        return hidden, outputs[-1] 
 
     def backward_pass():
-        pass
+        pass 
+
+    def train(self, X: torch.tensor, Y: torch.tensor):
+        for _ in range(self.epochs): # e = number of epoch we are at
+            for index, sequence in enumerate(X[:self.epochs]):
+                hidden, y_pred = self.forward_pass(sequence, Y[index])
+                # print(f'sequence: {sequence}')
+                # print(f'index: {index}, y value: {Y[index]}')
+                # print(hidden)
+                # print("-")
+                # print(output) # Y_hat
+                loss = 1/2 * ((Y[index] - y_pred) ** 2) # Squared error 
+                print(f'loss: {loss}')
+                
 
 def prepare_input():
     df = pd.read_csv('data/day_data.csv')
     # Normalized temperature in Celsius
     temperatures = df['temp'].tolist()
-    return temperatures[:100]
+
+    Xs, Ys = [], [] 
+
+    for i in range(len(temperatures)-TIME_STEPS):
+        Xs.append(temperatures[i:i+TIME_STEPS])
+        Ys.append(temperatures[i+TIME_STEPS])
+
+    Xs = torch.tensor(Xs)
+    Ys = torch.tensor(Ys)
+
+    # 80, 20 split 
+    number_sequences = Xs.shape[0]
+    train_size = int(number_sequences * 0.80)
+
+    X = Xs[:train_size]
+    Y = Ys[:train_size]
+
+    testX = Xs[train_size:]
+    testY = Ys[train_size:]
+
+    return X, Y, testX, testY
 
 def main():
-    temperatures = prepare_input()
+    X, Y, testX, testY = prepare_input()
     network = RNN()
-    RNN.forward_pass(network, temperatures)
+    network.train(X, Y)
 
 if __name__ == "__main__":
     main()
