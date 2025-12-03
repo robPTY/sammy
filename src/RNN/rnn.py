de67cc0a37f551c9069c960f27a8155273064b2f
robPTY <rcadown8@gmail.com>
2025-12-03 18:20:47 -0500

    added backward pass v0

--- src/RNN/rnn.py
+++ src/RNN/rnn.py
@@ -1,12 +1,10 @@
 import torch
-import pandas as pd
 from typing import Tuple
 import torch.nn.functional as F
 
 INPUT_DIMENSIONS = (1, 2)
 HIDDEN_DIMENSIONS = (2, 2)
 OUTPUT_DIMENSIONS = (2, 1)
-TIME_STEPS = 3
 
 class RNN:
     def __init__(self):
@@ -16,10 +14,10 @@ class RNN:
         self.OutputWeights = torch.randn(OUTPUT_DIMENSIONS, generator=gen)
         self.HiddenBias = torch.randn(HIDDEN_DIMENSIONS[1], generator=gen)
         self.OutputBias = torch.randn(OUTPUT_DIMENSIONS[1], generator=gen)
-        self.epochs = 100
+        self.epochs = 5
 
     def forward_pass(self, X_seq: torch.tensor, Y: torch.tensor) -> Tuple[torch.tensor, torch.tensor]:
-        previous_step = None
+        previous_hidden = None
         hidden = torch.zeros((X_seq.shape[0], self.InputWeights.shape[1]))
         outputs = torch.zeros((X_seq.shape[0], self.OutputWeights.shape[1]))
 
@@ -29,68 +27,66 @@ class RNN:
             X_i = X @ self.InputWeights
 
             # Hidden layer
-            if previous_step is None: 
+            if previous_hidden is None: 
                 Xh_t = F.relu(X_i)
             else:
-                X_h = previous_step @ self.HiddenWeights
+                X_h = previous_hidden @ self.HiddenWeights
                 Xh_t = F.tanh(X_i + X_h + self.HiddenBias)
-            previous_step = Xh_t
+            previous_hidden = Xh_t
 
             hidden[t, :] = Xh_t
             
             # Output layer
             logit = Xh_t @ self.OutputWeights + self.OutputBias # Y_hat
             outputs[t, :] = logit 
+            # print(outputs)
         
         # Return the hidden state for grad descent, output of the RNN 
-        return hidden, outputs[-1] 
+        return hidden, outputs # was outputs[-1]
 
-    def backward_pass():
-        pass 
+    def backward_pass(self, sequence, hidden, dy):
+        next_hidden = None
+        input_weight_grad, output_weight_grad, hidden_weight_grad, hidden_bias_grad, output_bias_grad = [0] * 5
+
+        for t in range(hidden.shape[0]-1, -1, -1):
+            output_bias_grad += dy # dL/dd 
+            output_weight_grad += (dy * hidden[t]) # dL/dWy
+            
+            print(f'W_y grad: {output_weight_grad}')
+            print(f'Output bias grad: {output_bias_grad}')
+            if next_hidden is not None:
+                h_grad = dy * self.OutputWeights.T + next_hidden
+            else:
+                h_grad = dy * self.OutputWeights.T 
+            # print(output_bias_grad.item())
+            tanh_grad = 1 - hidden[t]**2 # dh/dtanh
+            h_grad = h_grad * tanh_grad
+            next_hidden = h_grad 
+
+            if t > 0:
+                hidden_weight_grad += h_grad * hidden[t-1]
+            input_weight_grad += h_grad * sequence[t]
+            hidden_bias_grad += h_grad
+            print(f'W_h grad: {hidden_weight_grad}')
+            print(f'W_x grad: {input_weight_grad}')
+            print(f'Hidden bias grad: {hidden_bias_grad}')
+            print("------------------------------------------")
+
+            learning_rate = 1e-6
 
     def train(self, X: torch.tensor, Y: torch.tensor):
         for _ in range(self.epochs): # e = number of epoch we are at
             for index, sequence in enumerate(X[:self.epochs]):
-                hidden, y_pred = self.forward_pass(sequence, Y[index])
-                # print(f'sequence: {sequence}')
+                hidden, y_pred = self.forward_pass(sequence, Y[index]) # y_pred is all outputs
+                y_hat = y_pred[-1]
+                # print(f'sequence/index: {sequence}/{index}')
                 # print(f'index: {index}, y value: {Y[index]}')
                 # print(hidden)
                 # print("-")
-                # print(output) # Y_hat
-                loss = 1/2 * ((Y[index] - y_pred) ** 2) # Squared error 
-                print(f'loss: {loss}')
-                
 
-def prepare_input():
-    df = pd.read_csv('data/day_data.csv')
-    # Normalized temperature in Celsius
-    temperatures = df['temp'].tolist()
+                loss = 1/2 * ((y_hat - Y[index]) ** 2) # Squared error 
+                # print(f'loss: {loss.item()}')
 
-    Xs, Ys = [], [] 
+                dy = (y_hat - Y[index]) # dL/dy
 
-    for i in range(len(temperatures)-TIME_STEPS):
-        Xs.append(temperatures[i:i+TIME_STEPS])
-        Ys.append(temperatures[i+TIME_STEPS])
-
-    Xs = torch.tensor(Xs)
-    Ys = torch.tensor(Ys)
-
-    # 80, 20 split 
-    number_sequences = Xs.shape[0]
-    train_size = int(number_sequences * 0.80)
-
-    X = Xs[:train_size]
-    Y = Ys[:train_size]
-
-    testX = Xs[train_size:]
-    testY = Ys[train_size:]
-
-    return X, Y, testX, testY
-
-def main():
-    X, Y, testX, testY = prepare_input()
-    network = RNN()
-    network.train(X, Y)
-
-if __name__ == "__main__":
-    main()
+                self.backward_pass(sequence, hidden, dy)
