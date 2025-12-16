f8428e641fb9018a63356393ea8e37b09f90d284
robPTY <rcadown8@gmail.com>
2025-12-16 18:18:55 -0500

    added initial backward pass

--- src/LSTM/cell_block.py
+++ src/LSTM/cell_block.py
@@ -1,27 +1,35 @@
 import torch
 from gates import ForgetGate, InputGate, CandidateGate, OutputGate
 from activations import Tanh
+from typing import Tuple
+
+States = Tuple[torch.tensor, torch.tensor, torch.tensor, torch.tensor, 
+               torch.tensor, torch.tensor, torch.tensor, torch.tensor]
 
 class Cell:
-    def __init__(self, input_dims, hidden_dims):
+    def __init__(self, input_dims, hidden_dims, output_dims):
+        gen = torch.Generator().manual_seed(42)
         # Forget gate
-        self.Wf = torch.randn((input_dims + hidden_dims, hidden_dims)) * 0.01
-        self.bf = torch.randn((1, hidden_dims))
+        self.Wf = torch.randn((input_dims + hidden_dims, hidden_dims), generator=gen) * 0.1
+        self.bf = torch.randn((1, hidden_dims), generator=gen)
         self.Ft = ForgetGate()
         # Input gate
-        self.Wi = torch.randn((input_dims + hidden_dims, hidden_dims)) * 0.01
-        self.bi = torch.randn((1, hidden_dims))
+        self.Wi = torch.randn((input_dims + hidden_dims, hidden_dims), generator=gen) * 0.1
+        self.bi = torch.randn((1, hidden_dims), generator=gen)
         self.It = InputGate()
         # Candidate gate 
-        self.Wc = torch.randn((input_dims + hidden_dims, hidden_dims)) * 0.01
-        self.bc = torch.randn((1, hidden_dims))
+        self.Wc = torch.randn((input_dims + hidden_dims, hidden_dims), generator=gen) * 0.1
+        self.bc = torch.randn((1, hidden_dims), generator=gen)
         self.Ct = CandidateGate()
         # Output gate 
-        self.Wo = torch.randn((input_dims + hidden_dims, hidden_dims)) * 0.01
-        self.bo = torch.randn((1, hidden_dims))
+        self.Wo = torch.randn((input_dims + hidden_dims, hidden_dims), generator=gen) * 0.1
+        self.bo = torch.randn((1, hidden_dims), generator=gen)
         self.Ot = OutputGate()
+        
+        self.Wy = torch.randn((hidden_dims, output_dims), generator=gen) * 0.1
+        self.by = torch.randn((1, output_dims), generator=gen)
 
-    def forward(self, prev_ct: torch.tensor, prev_h: torch.tensor, x: torch.tensor):
+    def forward(self, prev_ct: torch.tensor, prev_h: torch.tensor, x: torch.tensor) -> States:
         Xt = torch.cat((x, prev_h), dim=1)
         ft = self.Ft.forward(self.Wf, Xt, self.bf)
 
@@ -38,4 +46,28 @@ class Cell:
         t = Tanh() 
         ht = ot * t.forward(cell_state)
 
-        return cell_state, ht
+        zt = ht @ self.Wy + self.by # of shape (1, 1)
+
+        return Xt, ft, it, ct, ot, cell_state, ht, zt
+
+    def backward(self, Xt: torch.tensor, ot: torch.tensor, cell_state: torch.tensor, 
+                 hidden_state: torch.tensor, dZ: torch.tensor, xt: torch.tensor):
+        T = hidden_state.shape[1]
+        tan = Tanh() 
+        dWf, dWi = torch.zeros_like(self.Wf), torch.zeros_like(self.Wi)
+        dWc, dWo = torch.zeros_like(self.Wc), torch.zeros_like(self.Wo)
+        dWy, dby = torch.zeros_like(self.Wy), torch.zeros_like(self.by)
+        dbo, dbc = torch.zeros_like(self.bo), torch.zeros_like(self.bc)
+        dbi, dbf = torch.zeros_like(self.bi), torch.zeros_like(self.bf)
+
+        dWy = dZ @ hidden_state # dZ/dWy
+        dby = dZ # dZ/dby
+
+        print(f'dWy: {dWy}')
+        print(f'dby: {dby}')
+        for t in range(T-1, -1, -1):
+            real_ot = ot * (1- ot)
+            dht = dZ @ self.Wy.T
+            
+            dWo += dht * tan.forward(cell_state) * real_ot @ Xt # dL/dWo
+            print(f'dWo: {dWo}')
--- src/LSTM/lstm.py
+++ src/LSTM/lstm.py
@@ -2,22 +2,55 @@ import torch
 from cell_block import Cell
 
 class LSTM: 
-    def __init__(self, input_dims, hidden_dims):
+    def __init__(self, input_dims, hidden_dims, output_dims):
         self.cell_dims = hidden_dims
-        self.cell = Cell(input_dims, hidden_dims)
+        self.output_dims = output_dims
+        self.cell = Cell(input_dims, hidden_dims, output_dims)
+        # self.Xt_hist = torch.zeros(T, input_dims + hidden_dims)
+        # self.ft_hist = torch.zeros(T, hidden_dims) 
+        # self.it_hist = torch.zeros(T, hidden_dims) 
+        # self.ct_hist = torch.zeros(T, hidden_dims)  
+        # self.ot_hist = torch.zeros(T, hidden_dims)  
+        # self.cell_hist = torch.zeros(T, hidden_dims) 
+        # self.h_hist = torch.zeros(T, hidden_dims)
 
     def forward(self, x_sequence: torch.tensor):
-        T = len(x_sequence)
+        T = x_sequence.shape[0]
 
         prev_hidden = torch.zeros(1, self.cell_dims)
         cell_state = torch.zeros(1, self.cell_dims)
+        z = torch.zeros(T, self.output_dims)
 
         for t in range(T):
             xt = x_sequence[t].view((1, -1))
-            self.cell.forward(cell_state, prev_hidden, xt)
+            Xt, ft, it, ct, ot, new_cell_state, hidden, zt = self.cell.forward(cell_state, prev_hidden, xt)
+            z[t] = zt.squeeze(0)
+            
+            # print(f'cell state at time {t}: {cell_state}')
+            # print(f'hidden_state at time {t}: {hidden}')
+            prev_hidden = hidden
+            cell_state = new_cell_state
         
-        return prev_hidden, cell_state
+        return Xt, ft, it, ct, ot, cell_state, prev_hidden, z
+    
+    def backward(self, Xt: torch.tensor,ot: torch.tensor, cell_state: torch.tensor, 
+                 hidden_state: torch.tensor, dZ: torch.tensor, x_sequence: torch.tensor):
+        self.cell.backward(Xt, ot, cell_state, hidden_state, dZ, x_sequence)
+    
+    def calculate_loss(self, Y_pred: torch.tensor, Y_true: torch.tensor) -> torch.tensor:
+        N = Y_pred.numel()
+        return torch.sum((Y_true - Y_pred)**2) / N
 
-    def train(self, X: torch.tensor):
+    def train(self, X: torch.tensor, Y: torch.tensor):
         for index, x_sequence in enumerate(X[:1]):
-            self.forward(x_sequence)
+            Xt, ft, it, ct, ot, cell_state, prev_hidden, y_pred = self.forward(x_sequence)
+            y_true = Y[index].view(-1, 1)
+
+            # print(x_sequence)
+            # print(y_true)
+            # print(y_pred)
+
+            dZ = (2/y_pred.numel() * (y_pred - y_true))
+            loss = self.calculate_loss(y_pred, y_true)
+            print(f'loss: {loss.item()}')
+            self.backward(Xt, ot, cell_state, prev_hidden, dZ, x_sequence)
